7938ba1b6f200a0d163d19ec2cafc2a64eff24c4
AlinaLina <crazy.musician@yandex.ru>
2023-12-29 23:44:34 +0400

    :rocket: UPDATE: херня дубль 2
    да

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -10,24 +10,20 @@
 
 <style lang="scss">
   * {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
+    padding: 0;
+    margin: 0;
   }
-  img {
-    z-index: 3;
-  }
+
   header {
     height: 100px;
-    background: url(img/logo.png) no-repeat;
     background: #1e3d6c;
-    z-index: 1;
   }
+
   .text {
-    color: black;
     position: absolute;
-    font-size: 40px;
     left: 197px;
-    z-index: 2;
+    font-size: 40px;
+    color: white;
   }
 </style>
